7df507d4c0043e91238ad8e698039e75057a8a2b
Thanh Nguyen <thanhnguyencs@utexas.edu>
2015-06-30 12:17:12 -0500

    add debug mode

--- src/AES.java
+++ src/AES.java
@@ -2,10 +2,22 @@
  * Created by thanhnguyencs on 6/28/15.
  */
 public class AES {
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
 
 
     public static void main(String[] args) {
+
+        if (args[0].equals("db")) {
+            // debug mode. Command line: java AES db <d or e> <key> <one line of plaintext>
+            AES.DEBUG = true;
+
+            String mode = args[1];
+            String key = args[2];
+            String state = args[3];
+
+            DataCrypto aes = new DataCrypto(key);
+            aes.setState(state);
+        }
 
         // parse command line
 
--- src/DataCrypto.java
+++ src/DataCrypto.java
@@ -2,9 +2,37 @@
  * Created by thanhnguyencs on 6/29/15.
  */
 public class DataCrypto {
+    private byte[][] state;
+    private keyCrypto keyObj;
+
 
     // constructor (parameter: string)
     // create object of class key (pass into: string key)
+
+    DataCrypto(String key) {
+        keyObj = new keyCrypto(key);
+        state = new byte[4][4];
+        // initial state
+        for (int i = 0; i < state[0].length; ++i) {
+            for (int j = 0;j < state.length; ++j) {
+                state[i][j] = 0;
+            }
+        }
+    }
+
+    // input the line containing state
+    public void setState(String line) {
+        int num = 0;
+        for (int i = 0; i < state[0].length; ++i) {
+            for (int j = 0;j < state.length; ++j) {
+                String twoChars = line.substring(num, num += 2);
+                state[i][j] = Byte.valueOf(twoChars, 16);
+            }
+        }
+    }
+
+
+
 
 
 
